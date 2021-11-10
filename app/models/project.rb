00a341112b88899713dd3ad3d6c6b029ef492370
Chad Bowman <chadbowman1909@gmail.com>
2021-11-10 22:04:48 +0000

    break test

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
     # requires the title and description 
-    validates_presence_of :title
-    validates_presence_of :description
+    #validates_presence_of :title
+    #validates_presence_of :description
 end
