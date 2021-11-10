9855c24d7f4da40d63aef8ac97cd5e08ddd77c1f
Chad Bowman <chadbowman1909@gmail.com>
2021-11-10 21:32:30 +0000

    push

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,4 @@
 class Project < ApplicationRecord
     # requires the title and description 
-    validates_presence_of :title
-    validates_presence_of :description
+    validates_presence_of :title, :descriptiongit 
 end
